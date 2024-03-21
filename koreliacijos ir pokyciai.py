312c9c46b715cf357d5621bb5e818572daf04c91
LoretaE <e.loreta@gmail.com>
2024-03-21 12:56:13 +0200

    Koreliacijos - metų ir sveikatos indekso priklausomybė

--- koreliacijos ir pokyciai.py
+++ koreliacijos ir pokyciai.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 pd.set_option('display.max_columns', 10)
 pd.set_option('display.max_rows', 500)
@@ -19,4 +20,18 @@ skandinavijos_saliu_indeksai = df.loc[(df['country'] == 'Sweden') | (df['country
 skandinavijos_saliu_koreliacija = (skandinavijos_saliu_indeksai['year'].corr(skandinavijos_saliu_indeksai['value']).
                                    round(2))
 print('Skandinavijos šalių sveikatos indekso priklausomybė nuo metų: ', skandinavijos_saliu_koreliacija)
-print('\n')
+
+bendras_saliu_pokytis = df.groupby('year')['value'].mean().round(2).pct_change()
+baltijos_saliu_pokytis = baltijos_saliu_indeksai.groupby('year')['value'].mean().round(2).pct_change()
+skandinavijos_saliu_pokytis = (skandinavijos_saliu_indeksai.groupby('year')['value'].mean().round(2).pct_change()) * 100
+
+plt.figure(figsize=(14,8))
+plt.plot(bendras_saliu_pokytis, label='Visų šalių')
+plt.plot(baltijos_saliu_pokytis, label='Baltijos šalių')
+plt.plot(skandinavijos_saliu_pokytis, label='Skandinavijos šalių')
+
+plt.title('Sveikatos indekso pokytis pagal metus')
+plt.xlabel('Metai')
+plt.ylabel('Pokytis proc.')
+plt.legend()
+plt.show()
