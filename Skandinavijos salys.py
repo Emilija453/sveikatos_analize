d43f7057c66cc7bf54b47da08b7645c2f4a572e4
rasadzen <163419930+rasadzen@users.noreply.github.com>
2024-03-21 12:26:06 +0200

    betkas

---
+++ Skandinavijos salys.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv('health-index-1.csv')
+# print(df)
+
+### Skandinavijos šalys
+
+skandinavijos_salys = df.loc[(df['country'] == 'Denmark') | (df['country'] == 'Sweden') | (df['country'] == 'Norway')]
+# print(skandinavijos_salys)
+
+periodas_1980_1985 = skandinavijos_salys[(skandinavijos_salys['year'] >= 1980) & (skandinavijos_salys['year'] <= 1985)]
+print(periodas_1980_1985)
+
+saliu_indekso_vidurkis_1980_1985 = periodas_1980_1985.groupby('country')['value'].mean()
+print(saliu_indekso_vidurkis_1980_1985)
