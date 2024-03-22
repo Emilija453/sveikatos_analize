f72e3ced8861eb34788bdad206cc0af09652ee4a
LoretaE <e.loreta@gmail.com>
2024-03-22 10:43:07 +0200

    bendras testavimui

--- bendras_testavimui.py
+++ bendras_testavimui.py
@@ -7,7 +7,7 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.width', 500)
 
 def df_from_csv(csv_file):
-    df = pd.read_csv('health-index-1.csv')
+    df = pd.read_csv(csv_file)
     return df
 
 def bendras_indeksas(df):
@@ -25,26 +25,51 @@ def bendras_indeksas(df):
     print('Bendras visų šalių sveikatos indeksas\n', bendro_indekso_df)
     return bendro_indekso_df
 
-"""SKANDINAVIJOS ŠALIŲ SVEIKATOS INDEKSO VIDURKIS SKIRTINGAIS LAIKOTARPIAIS"""
-skandinavijos_salys = df.loc[(df['country'] == 'Denmark') | (df['country'] == 'Sweden') | (df['country'] == 'Norway')]
-periodas_1980_1985 = skandinavijos_salys[(skandinavijos_salys['year'] >= 1980) & (skandinavijos_salys['year'] <= 1985)]
-periodas_1990_1995 = skandinavijos_salys[(skandinavijos_salys['year'] >= 1990) & (skandinavijos_salys['year'] <= 1995)]
-periodas_2000_2005 = skandinavijos_salys[(skandinavijos_salys['year'] >= 2000) & (skandinavijos_salys['year'] <= 2005)]
-periodas_2010_2013 = skandinavijos_salys[(skandinavijos_salys['year'] >= 2010) & (skandinavijos_salys['year'] <= 2013)]
 
-saliu_indekso_vidurkis_1980_1985 = periodas_1980_1985.groupby('country')['value'].mean().round(2)
-saliu_indekso_vidurkis_1990_1995 = periodas_1990_1995.groupby('country')['value'].mean().round(2)
-saliu_indekso_vidurkis_2000_2005 = periodas_2000_2005.groupby('country')['value'].mean().round(2)
-saliu_indekso_vidurkis_2010_2013 = periodas_2010_2013.groupby('country')['value'].mean().round(2)
+def baltijos_saliu_indeksas(df):
+    baltijos_salys = df.loc[(df['country'] == 'Lithuania') | (df['country'] == 'Latvia') | (df['country'] == 'Estonia')]
 
-skand1 = pd.merge(saliu_indekso_vidurkis_1980_1985, saliu_indekso_vidurkis_1990_1995, on='country')
-skand2 = pd.merge(saliu_indekso_vidurkis_2000_2005, saliu_indekso_vidurkis_2010_2013, on='country')
-skandinavijos_saliu_indekso_df = pd.merge(skand1, skand2, on='country').transpose()
-skandinavijos_saliu_indekso_df.rename(index={'value_x_x': '1980-1985', 'value_y_x': '1990-1995',
+    laikotarpis_1980_1985 = baltijos_salys[(baltijos_salys['year'] >= 1980) & (baltijos_salys['year'] <= 1985)]
+    laikotarpis_1990_1995 = baltijos_salys[(baltijos_salys['year'] >= 1990) & (baltijos_salys['year'] <= 1995)]
+    laikotarpis_2000_2005 = baltijos_salys[(baltijos_salys['year'] >= 2000) & (baltijos_salys['year'] <= 2005)]
+    laikotarpis_2010_2013 = baltijos_salys[(baltijos_salys['year'] >= 2010) & (baltijos_salys['year'] <= 2013)]
+
+    baltijos_saliu_indekso_vidurkis_1980_1985 = laikotarpis_1980_1985.groupby('country')['value'].mean().round(2)
+    baltijos_saliu_indekso_vidurkis_1990_1995 = laikotarpis_1990_1995.groupby('country')['value'].mean().round(2)
+    baltijos_saliu_indekso_vidurkis_2000_2005 = laikotarpis_2000_2005.groupby('country')['value'].mean().round(2)
+    baltijos_saliu_indekso_vidurkis_2010_2013 = laikotarpis_2010_2013.groupby('country')['value'].mean().round(2)
+
+    balt1 = pd.merge(baltijos_saliu_indekso_vidurkis_1980_1985, baltijos_saliu_indekso_vidurkis_1990_1995, on='country')
+    balt2 = pd.merge(baltijos_saliu_indekso_vidurkis_2000_2005, baltijos_saliu_indekso_vidurkis_2010_2013, on='country')
+    baltijos_saliu_indekso_df = pd.merge(balt1, balt2, on='country').transpose()
+    baltijos_saliu_indekso_df.rename(index={'value_x_x': '1980-1985', 'value_y_x': '1990-1995', 'value_x_y': '2000-2005',
+                                            'value_y_y': '2010-2013'}, inplace=True)
+    baltijos_saliu_indekso_df.rename(columns={'Estonia': 'Estija', 'Latvia': 'Latvija', 'Lithuania': 'Lietuva'},
+                                     inplace=True)
+    print('\nBaltijos šalių sveikatos indeksas\n', baltijos_saliu_indekso_df)
+    return baltijos_saliu_indekso_df
+
+
+def skandinavijos_saliu_indeksas(df):
+    skandinavijos_salys = df.loc[(df['country'] == 'Denmark') | (df['country'] == 'Sweden') | (df['country'] == 'Norway')]
+    periodas_1980_1985 = skandinavijos_salys[(skandinavijos_salys['year'] >= 1980) & (skandinavijos_salys['year'] <= 1985)]
+    periodas_1990_1995 = skandinavijos_salys[(skandinavijos_salys['year'] >= 1990) & (skandinavijos_salys['year'] <= 1995)]
+    periodas_2000_2005 = skandinavijos_salys[(skandinavijos_salys['year'] >= 2000) & (skandinavijos_salys['year'] <= 2005)]
+    periodas_2010_2013 = skandinavijos_salys[(skandinavijos_salys['year'] >= 2010) & (skandinavijos_salys['year'] <= 2013)]
+    saliu_indekso_vidurkis_1980_1985 = periodas_1980_1985.groupby('country')['value'].mean().round(2)
+    saliu_indekso_vidurkis_1990_1995 = periodas_1990_1995.groupby('country')['value'].mean().round(2)
+    saliu_indekso_vidurkis_2000_2005 = periodas_2000_2005.groupby('country')['value'].mean().round(2)
+    saliu_indekso_vidurkis_2010_2013 = periodas_2010_2013.groupby('country')['value'].mean().round(2)
+
+    skand1 = pd.merge(saliu_indekso_vidurkis_1980_1985, saliu_indekso_vidurkis_1990_1995, on='country')
+    skand2 = pd.merge(saliu_indekso_vidurkis_2000_2005, saliu_indekso_vidurkis_2010_2013, on='country')
+    skandinavijos_saliu_indekso_df = pd.merge(skand1, skand2, on='country').transpose()
+    skandinavijos_saliu_indekso_df.rename(index={'value_x_x': '1980-1985', 'value_y_x': '1990-1995',
                                              'value_x_y': '2000-2005', 'value_y_y': '2010-2013'}, inplace=True)
-skandinavijos_saliu_indekso_df.rename(columns={'Denmark': 'Danija', 'Norway': 'Norvegija',
-                                               'Sweden': 'Švedija'}, inplace=True)
-print('\nSkandinavijos šalių sveikatos indeksas\n', skandinavijos_saliu_indekso_df)
+    skandinavijos_saliu_indekso_df.rename(columns={'Denmark': 'Danija', 'Norway': 'Norvegija',
+                                                   'Sweden': 'Švedija'}, inplace=True)
+    print('\nSkandinavijos šalių sveikatos indeksas\n', skandinavijos_saliu_indekso_df)
+    return skandinavijos_saliu_indekso_df
 
 
 
@@ -52,32 +77,11 @@ print('\nSkandinavijos šalių sveikatos indeksas\n', skandinavijos_saliu_indeks
 csv_file = 'health-index-1.csv'
 df = df_from_csv(csv_file)
 bendras_indeksas(df)
+skandinavijos_saliu_indeksas(df)
+baltijos_saliu_indeksas(df)
 
 
 
-
-
-# """BALTIJOS ŠALIŲ SVEIKATOS INDEKSO VIDURKIS SKIRTINGAIS LAIKOTARPIAIS"""
-# baltijos_salys = df.loc[(df['country'] == 'Lithuania') | (df['country'] == 'Latvia') | (df['country'] == 'Estonia')]
-#
-# laikotarpis_1980_1985 = baltijos_salys[(baltijos_salys['year'] >= 1980) & (baltijos_salys['year'] <= 1985)]
-# laikotarpis_1990_1995 = baltijos_salys[(baltijos_salys['year'] >= 1990) & (baltijos_salys['year'] <= 1995)]
-# laikotarpis_2000_2005 = baltijos_salys[(baltijos_salys['year'] >= 2000) & (baltijos_salys['year'] <= 2005)]
-# laikotarpis_2010_2013 = baltijos_salys[(baltijos_salys['year'] >= 2010) & (baltijos_salys['year'] <= 2013)]
-#
-# baltijos_saliu_indekso_vidurkis_1980_1985 = laikotarpis_1980_1985.groupby('country')['value'].mean().round(2)
-# baltijos_saliu_indekso_vidurkis_1990_1995 = laikotarpis_1990_1995.groupby('country')['value'].mean().round(2)
-# baltijos_saliu_indekso_vidurkis_2000_2005 = laikotarpis_2000_2005.groupby('country')['value'].mean().round(2)
-# baltijos_saliu_indekso_vidurkis_2010_2013 = laikotarpis_2010_2013.groupby('country')['value'].mean().round(2)
-#
-# balt1 = pd.merge(baltijos_saliu_indekso_vidurkis_1980_1985, baltijos_saliu_indekso_vidurkis_1990_1995, on='country')
-# balt2 = pd.merge(baltijos_saliu_indekso_vidurkis_2000_2005, baltijos_saliu_indekso_vidurkis_2010_2013, on='country')
-# baltijos_saliu_indekso_df = pd.merge(balt1, balt2, on='country').transpose()
-# baltijos_saliu_indekso_df.rename(index={'value_x_x': '1980-1985', 'value_y_x': '1990-1995', 'value_x_y': '2000-2005',
-#                                         'value_y_y': '2010-2013'}, inplace=True)
-# baltijos_saliu_indekso_df.rename(columns={'Estonia': 'Estija', 'Latvia': 'Latvija', 'Lithuania': 'Lietuva'},
-#                                  inplace=True)
-# print('\nBaltijos šalių sveikatos indeksas\n', baltijos_saliu_indekso_df)
 #
 # """SVEIKATOS INDEKSŲ TENDENCIJŲ GRAFIKAS"""
 # plt.figure(figsize=(14, 8))
