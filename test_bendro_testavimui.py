8d950d364841e40b3c3dea7f58fd96451524e6be
rasadzen <163419930+rasadzen@users.noreply.github.com>
2024-03-22 13:44:58 +0200

    testuojam

--- test_bendro_testavimui.py
+++ test_bendro_testavimui.py
@@ -10,8 +10,3 @@ def test_df_from_csv():
     assert len(result) == expected_response
 
 
-def test_baltijos_saliu_indeksas():
-    df_baltic = 'test_Baltic_counrties.csv'
-    df = pd.read_csv(df_baltic)
-    expected_result = df
-    assert baltijos_saliu_indeksas(df_baltic) == expected_result
