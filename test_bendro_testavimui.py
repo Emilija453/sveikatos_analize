51035683ac324669f069156ff1d148c9b46cfab8
rasadzen <163419930+rasadzen@users.noreply.github.com>
2024-03-22 12:15:25 +0200

    naujas csv

---
+++ test_bendro_testavimui.py
@@ -0,0 +1,12 @@
+import pytest
+import pandas as pd
+from bendras_testavimui import df_from_csv
+
+@pytest.fixture
+
+def test_df_from_csv():
+    df_baltic = 'test_Baltic_counrties.csv'
+    result = df_from_csv(df_baltic)
+    expected_response = 39
+    assert len(result) == expected_response
+
