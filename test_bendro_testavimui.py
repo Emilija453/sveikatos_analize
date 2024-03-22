300727716c4a9853b6b228e0b03be8e2422fbe8c
rasadzen <163419930+rasadzen@users.noreply.github.com>
2024-03-22 12:46:25 +0200

    naujas csv

--- test_bendro_testavimui.py
+++ test_bendro_testavimui.py
@@ -1,6 +1,6 @@
 import pytest
 import pandas as pd
-from bendras_testavimui import df_from_csv
+from bendras_testavimui import df_from_csv, baltijos_saliu_indeksas
 
 
 def test_df_from_csv():
@@ -9,3 +9,9 @@ def test_df_from_csv():
     expected_response = 39
     assert len(result) == expected_response
 
+
+def test_baltijos_saliu_indeksas():
+    df_baltic = 'test_Baltic_counrties.csv'
+    df = pd.read_csv(df_baltic)
+    expected_result = df
+    assert baltijos_saliu_indeksas(df_baltic) == expected_result
