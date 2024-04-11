ab691c0f22dc9f2cd5a68fb9b6353aa1f1095301
rasadzen <163419930+rasadzen@users.noreply.github.com>
2024-04-11 12:27:03 +0300

    ....

--- bendras_II_roc_auc.py
+++ bendras_II_roc_auc.py
@@ -29,7 +29,7 @@ clf.fit(X_train, y_train)
 y_scores = clf.predict_proba(X_test)[:, 1]
 fpr, tpr, thresholds = roc_curve(y_test, y_scores)
 roc_auc = auc(fpr, tpr)
-print(f"ROC AUC: {roc_auc}")
+print(f"AUC: {roc_auc}")
 
 plt.figure()
 plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area =%0.2f)' % roc_auc)
